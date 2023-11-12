74d80b25b7d4d196218222551baf481603753448
Woomymy <woomy@woomy.be>
2023-11-12 13:36:18 +0100

    camellia: Link media C2 service against v33 `libstagefright_foundation`
    * Fixes crashes and high CPU usage
    
    Change-Id: I60c07d10c2db9335d20c6dbaaaeacc9313aebf06
    Signed-off-by: Woomymy <woomy@woomy.be>
    Signed-off-by: ramshell688 <ramonzost68@gmail.com>

--- extract-files.sh
+++ extract-files.sh
@@ -73,6 +73,9 @@ function blob_fixup {
             ;;
         vendor/etc/init/vendor.mediatek.hardware.mtkpower@1.0-service.rc)
             echo "$(cat ${2}) input" > "${2}"
+	    ;;
+        vendor/bin/hw/android.hardware.media.c2@1.2-mediatek)
+            "${PATCHELF}" --add-needed "libstagefright_foundation-v33.so" "${2}"
             ;;
 	vendor/lib*/libmtkcam_stdutils.so)
             "${PATCHELF}" --replace-needed "libutils.so" "libutils-v32.so" "$2"
