224a900332a66cb53e743eebab903476ed3523e3
ramshell688 <ramonzost68@gmail.com>
2023-11-14 18:49:53 +0700

    camellia: Welcome to MatrixxUI
    Signed-off-by: ramshell688 <ramonzost68@gmail.com>

--- lineage_camellia.mk
+++ lineage_camellia.mk
@@ -11,14 +11,19 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from device makefile.
 $(call inherit-product, device/xiaomi/camellia/device.mk)
 
-# Inherit some common PixelExperience stuff.
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+# Inherit some common MatrixxUI stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+MATRIXX_MAINTAINER := ramshell688
+MATRIXX_CHIPSET := MT6833
+MATRIXX_BATTERY := 5000mAh
+MATRIXX_DISPLAY := 1080x2400
 
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_USES_AOSP_RECOVERY := true
 TARGET_SUPPORTS_QUICK_TAP := true
 
-PRODUCT_NAME := aosp_camellia
+PRODUCT_NAME := matrixx_camellia
 PRODUCT_DEVICE := camellia
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := Redmi
